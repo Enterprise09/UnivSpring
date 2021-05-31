0ac173dc20114cd911802d3d99d05376a841a9ca
Enterprise09 <goldenstaytes@gmail.com>
2021-05-31 11:35:03 +0900

    Create userInfo1 jsp

--- spring0302-pjt0604/src/main/java/yuhan/spring/modelattr/userController.java
+++ spring0302-pjt0604/src/main/java/yuhan/spring/modelattr/userController.java
@@ -3,8 +3,11 @@ package yuhan.spring.modelattr;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class userController {
@@ -29,6 +32,18 @@ public class userController {
 		else {
 			return new UserVo();
 		}		
+	}
+	
+	
+	@RequestMapping(value="/userSave", method = RequestMethod.POST)
+	public ModelAndView userSave(@ModelAttribute("userVo") UserVo userVo, Model model){
+		model.addAttribute("msg", "회원정보 출력 성공");
+
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("user/userInfo1");
+		modelAndView.addObject("userVo", userVo);
+		
+		return modelAndView;
 	}
 
 }
