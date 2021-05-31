bf3ff8456764b690bf08f99e6924f5eab6476c3f
Enterprise09 <goldenstaytes@gmail.com>
2021-05-31 11:51:23 +0900

    Implement Controller

--- spring0302-pjt0605/src/main/java/yuhan/spring/attr/MyController.java
+++ spring0302-pjt0605/src/main/java/yuhan/spring/attr/MyController.java
@@ -1,8 +1,26 @@
 package yuhan.spring.attr;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class MyController {
 	
+	@RequestMapping("/form")
+	public String userForm() {
+		
+		
+		return "/form/userForm2";
+	}
 	
+	
+	@RequestMapping("/userInfo")
+	public String userInfor(
+			@ModelAttribute("userInfo") StudentInformation studentInformation) {
+		
+			
+		
+		return "form/userInfo2";
+	}
 	
 	
 
